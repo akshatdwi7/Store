f9acab08f2f14a266b383ceedabf8dbc36227738
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-10-05 22:21:27 +0530

    first commit

---
+++ src/test/java/com/testing/Store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
